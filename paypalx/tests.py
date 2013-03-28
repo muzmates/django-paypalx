8cd41cfee86ef65af47e086aafca360e042b0082
Max E. Kuznecov <mek@mek.uz.ua>
2013-03-28 04:07:47 +0200

    Add more tests

--- paypalx/tests.py
+++ paypalx/tests.py
@@ -11,14 +11,11 @@ from django.test import TestCase
 
 import paypalx
 
-#from django.utils.timezone import now
-#from django.core.urlresolvers import reverse
-#from django.contrib.auth.models import User
-
 SETTINGS = {"PPX_USER": "",
             "PPX_PWD": "",
             "PPX_SIGNATURE": "",
             "PPX_DEFAULT_CURRENCY": "USD",
+            "PPX_DEFAULT_DESCRIPTION": "Fake payment",
             "PPX_RETURN_URL": "",
             "PPX_CANCEL_URL": "",
             "PPX_USE_SANDBOX": True}
@@ -30,14 +27,28 @@ def mconf(val):
 
 class TestInit(TestCase):
     @mock.patch('paypalx.lib.conf', mconf)
+    @mock.patch('paypalx.lib.call_api', lambda _: {"ACK": "Success",
+                                                   "TOKEN": "MY_TOKEN"})
     def test_correct_init(self):
-        pass
+        tr = paypalx.init(1)
+
+        self.assertIsInstance(tr, paypalx.models.Transaction)
+        self.assertEqual("MY_TOKEN", tr.token)
+        self.assertIsNotNone(tr.date_after_set)
+        self.assertIsNotNone(tr.set_ec_correlation_id)
 
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     @mock.patch('paypalx.lib.conf', mconf)
     @mock.patch('paypalx.lib.call_api', lambda _: {"ACK": "ERROR"})
     def test_set_ec_not_ack(self):
+        self.assertRaises(Exception, paypalx.init, 1)
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    @mock.patch('paypalx.lib.conf', mconf)
+    @mock.patch('paypalx.lib.call_api', lambda _: {"ACK": "Success"})
+    def test_set_ec_no_token(self):
         self.assertRaises(Exception, paypalx.init, 1)
 
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
